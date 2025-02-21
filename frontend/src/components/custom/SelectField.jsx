f929bde0c60ddf43ef7a91187bcca047363c32f7
MNL\srmailom <mailom.sr@healthway.com.ph>
2025-02-21 08:55:51 +0800

    refactor folder structure - add frontend and backend folder

---
+++ frontend/src/components/custom/SelectField.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SelectField = () => {
+  return (
+    <div>
+      <select 
+        className='h-8 w-40 p-2 outline-none border-solid shadow-lg rounded-md'> 
+      </select>
+    </div>
+  )
+}
+
+export default SelectField
